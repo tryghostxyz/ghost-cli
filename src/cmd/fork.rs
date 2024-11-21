30e84388a829ad362cb6b489f5473ca6a045785e
Chris Chang <me@chrischang.io>
2024-11-20 21:19:43 -0500

    feat(fork): make id and path optional. id can now be read from current directory

--- src/cmd/fork.rs
+++ src/cmd/fork.rs
@@ -1,7 +1,7 @@
 use std::path::PathBuf;
 
 use clap::{Parser, ValueHint};
-use eyre::eyre;
+use eyre::{eyre, OptionExt};
 
 use crate::api_service::ApiService;
 use crate::types::{ForkRequest, GraphConfig};
@@ -10,12 +10,16 @@ use crate::utils::{check_and_create_dir, write_sources_and_conf};
 #[derive(Clone, Debug, Default, Parser)]
 pub struct ForkCmd {
     /// Fork into this directory
-    #[arg(value_hint = ValueHint::DirPath, value_name = "PATH")]
+    #[arg(
+        value_hint = ValueHint::DirPath,
+        value_name = "PATH",
+        default_value = "."
+    )]
     pub dir: PathBuf,
 
     /// ID of GhostGraph to fork
     #[arg(long)]
-    pub id: String,
+    pub id: Option<String>,
 
     /// Name for this GhostGraph. (defaults to dir name if not provided)
     #[arg(long, short)]
@@ -28,21 +32,27 @@ pub struct ForkCmd {
 
 impl ForkCmd {
     pub async fn run(self, api: &ApiService) -> eyre::Result<()> {
-        let Self { mut dir, id, name, replace } = self;
-        println!("Forking graph with ID: {}", id);
-        if replace {
-            GraphConfig::read(PathBuf::from("config.json"))
+        let (dir, id) = if self.replace {
+            let conf = GraphConfig::read(PathBuf::from("config.json"))
                 .map_err(|_| eyre!("cannot read config.json. Must be in a Ghost directory"))?;
-            dir = PathBuf::from(".");
+            (PathBuf::from("."), self.id.or(Some(conf.id)))
         } else {
-            check_and_create_dir(&dir)?;
-        }
-        let name = name
-            .or_else(|| dir.file_name().and_then(|os_str| os_str.to_str()).map(|s| s.to_string()));
+            check_and_create_dir(&self.dir)?;
+            (self.dir, self.id)
+        };
+
+        let id = id.ok_or_eyre("must pass --id if not --replace")?;
+        println!("Forking graph with ID: {}", id);
+        let name = self
+            .name
+            .or_else(|| dir.file_name().and_then(|os_str| os_str.to_str()).map(String::from));
+
         let resp = api.fork_graph(&id, &ForkRequest { name }).await?;
         println!("Graph has been successfully forked. Setting up local files...");
+
         write_sources_and_conf(&dir, resp.id, resp.version_id, None, resp.sources)?;
         println!("done! Check the {:?} directory", dir);
+
         Ok(())
     }
 }
